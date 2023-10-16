1f4034689131b39708feeb48684c79fa22b7e50d
jiwagon <jiwagon11@gmail.com>
2023-10-15 22:33:31 -0400

    functional data source assignment

--- ios101-project5-tumblr/ViewController.swift
+++ ios101-project5-tumblr/ViewController.swift
@@ -8,21 +8,28 @@ import Nuke
 
 class ViewController: UIViewController, UITableViewDataSource {
     
+    private var posts: [Post] = []
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("🍏 numberOfRowsInSection called with posts count: \(posts.count)")
        // Return the number of rows for the table.
-       return 50
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // Create the cell
+        
+        print("🍏 cellForRowAt called for row: \(indexPath.row)")
+        
+        //Create the Cell
         let cell = UITableViewCell()
+        
+        // Get the post-associated table view row
+            let post = posts[indexPath.row]
+        
+            // Configure the cell
+            cell.textLabel?.text = post.summary
 
-        // Configure the cell (i.e. update UI elements like labels, image views, etc.)
-        // Get the row where the cell will be placed using the `row` property on the passed in `indexPath` (i.e., `indexPath.row`)
-            cell.textLabel?.text = "Row \(indexPath.row)"
-
-        // Return the cell for use in the respective table view row
+            // Return the cell for use in the respective table view row
             return cell
     }
     
@@ -68,6 +75,10 @@ class ViewController: UIViewController, UITableViewDataSource {
                     for post in posts {
                         print("🍏 Summary: \(post.summary)")
                     }
+                    self?.posts = posts
+                    // tell the table view to reload data anytime we get more data and the table view will call all of its data source methods again which will give it a chance to incorporate our updated data.
+                    self?.postsTableView.reloadData()
+                    print("🍏 Fetched and stored \(posts.count) posts")
                 }
 
             } catch {
